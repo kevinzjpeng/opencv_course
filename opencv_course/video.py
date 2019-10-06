9416fd3789132f850d9ff2fd0189984f4486aba9
Bbot <bbot@beets3d.cn>
2019-10-06 11:55:11 +0800

    Sync fiels

--- opencv_course/video.py
+++ opencv_course/video.py
@@ -16,21 +16,16 @@ class Camera(SingletonConfigurable):
     fps = traitlets.Float(default_value=2.0).tag(config=True)
     capture_width = traitlets.Integer(default_value=3280).tag(config=True)
     capture_height = traitlets.Integer(default_value=2464).tag(config=True)
-<<<<<<< HEAD
     is_usb = traitlets.Bool(default_value=False).tag(config=True)
     brightness = traitlets.Float(default_value=10.0).tag(config=True)
-=======
->>>>>>> a9ecb41cffb7f9ed04fc3a3e0404b7a56955a240
     
     def __init__(self, *args, **kwargs):
         super(Camera, self).__init__(*args, **kwargs)
         self.value = np.empty((self.height, self.width, 2), dtype=np.uint8)
 
         try:
-<<<<<<< HEAD
             if self.is_usb:
                 self.cap = cv2.VideoCapture(1)
-#                 self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
                 if self.fps != 2.0:
                     self.cap.set(cv2.CAP_PROP_FPS, self.fps)
                 self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
@@ -47,9 +42,6 @@ class Camera(SingletonConfigurable):
             re, image = self.cap.read()
             print("h, w: {}, {}".format(image.shape[0], image.shape[1]))
             
-=======
-            self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
-            
             re, image = self.cap.read()
     
             if not re:
@@ -92,79 +84,6 @@ class Camera(SingletonConfigurable):
     def restart(self):
         self.stop()
         self.start()
-        
-class Camera2(SingletonConfigurable):
-    
-    value = traitlets.Any()
-    
-    # config
-    width = traitlets.Integer(default_value=224).tag(config=True)
-    height = traitlets.Integer(default_value=224).tag(config=True)
-    fps = traitlets.Integer(default_value=21).tag(config=True)
-    capture_width = traitlets.Integer(default_value=3280).tag(config=True)
-    capture_height = traitlets.Integer(default_value=2464).tag(config=True)
-    
-    def __init__(self, *args, **kwargs):
-        self.value = np.empty((self.height, self.width, 2), dtype=np.uint8)
-        super(Camera2, self).__init__(*args, **kwargs)
-
-        try:
-            self.cap = cv2.VideoCapture(0)
-
-            re, image = self.cap.read()
-            
->>>>>>> a9ecb41cffb7f9ed04fc3a3e0404b7a56955a240
-            if not re:
-                raise RuntimeError('Could not read image from camera.')
-
-#             if self.is_usb:
-#                 image = cv2.resize(image, (int(self.width), int(self.height)))
-            
-            self.camera = self
-            self.value = image
-            self.start()
-        except:
-            self.stop()
-            raise RuntimeError(
-                'Could not initialize camera.  Please see error trace.')
-
-        atexit.register(self.stop)
-
-    def _capture_frames(self):
-        while True:
-            re, image = self.cap.read()
-            if re:
-#                 if self.is_usb:
-#                     image = cv2.resize(image, (int(self.width), int(self.height)))
-                self.value = image
-            else:
-                break
-                
-    def _gst_str(self):
-        return 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
-                self.capture_width, self.capture_height, self.fps, self.width, self.height)
-    
-    def start(self):
-        if not self.cap.isOpened():
-            self.cap.open(self._gst_str(), cv2.CAP_GSTREAMER)
-        if not hasattr(self, 'thread') or not self.thread.isAlive():
-            self.thread = threading.Thread(target=self._capture_frames)
-            self.thread.start()
-
-    def stop(self):
-        if hasattr(self, 'cap'):
-            self.cap.release()
-        if hasattr(self, 'thread'):
-            self.thread.join()
-            
-    def restart(self):
-        self.stop()
-<<<<<<< HEAD
-        self.start()         
-=======
-        self.start()
-              
->>>>>>> a9ecb41cffb7f9ed04fc3a3e0404b7a56955a240
         
 class Video(SingletonConfigurable):
     
